5c1e6545c75b9530319866c6ec46ad85cc7468ab
James S <jamests1000@gmail.com>
2023-02-17 15:40:20 -0500

    added about me content and styling

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -1,16 +1,31 @@
 import React from 'react';
 
+const styles = {
+  p: {
+    margin: 32,
+    fontSize: 24
+  }
+}
+
 export default function About() {
   return (
     <div>
-      <h1>About Page</h1>
-      <p>
-        Nunc pharetra finibus est at efficitur. Praesent sed congue diam.
-        Integer gravida dui mauris, ut interdum nunc egestas sed. Aenean sed
-        mollis diam. Nunc aliquet risus ac finibus porta. Nam quis arcu non
-        lectus tincidunt fermentum. Suspendisse aliquet orci porta quam semper
-        imperdiet. Praesent euismod mi justo, faucibus scelerisque risus cursus
-        in. Sed rhoncus mollis diam, sit amet facilisis lectus blandit at.
+      <div className="container flex-column justify-space-between-lg justify-center align-center text-center" >
+        <h1>About Me</h1>
+
+      </div>
+
+      <p style={styles.p} className="text-center">
+      Hello and welcome to my About Me page!
+
+My name is James Simpson, and I'm a web developer based in the United States. I've been passionate about coding and web development since I was young, and I've spent the last few years honing my skills and building websites for clients of all types.
+
+I specialize in front-end web development, but I'm also proficient in back-end development and have experience with a variety of programming languages and frameworks, including HTML, CSS, JavaScript, React, Node.js, and more. I enjoy taking on challenging projects and working with clients to bring their vision to life.
+
+When I'm not coding, I enjoy staying up-to-date on the latest web development trends and attending industry conferences and events. I'm always looking for ways to improve my skills and stay ahead of the curve.
+
+If you're interested in working with me, please don't hesitate to reach out. I'm always excited to take on new projects and help businesses and individuals create beautiful, functional websites that meet their needs and exceed their expectations. Thank you for visiting my page, and I hope to hear from you soon!
+
       </p>
     </div>
   );
