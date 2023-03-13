7b09e6dd7cbae6acc25cc020a6381769c28fc9ed
James S <jamests1000@gmail.com>
2023-03-13 09:11:51 -0400

    added picture to note taker card

--- src/components/pages/Portfolio.js
+++ src/components/pages/Portfolio.js
@@ -1,5 +1,8 @@
 import React from 'react';
+import { CardImg } from 'react-bootstrap';
 import Card from 'react-bootstrap/Card';
+
+const noteTakerPicture = new URL("../images/noteTaker.png", import.meta.url)
 
 export default function Portfolio() {
   return (
@@ -21,9 +24,37 @@ export default function Portfolio() {
             <Card.Link href="https://github.com/jsimps39/work-day-schedulerjs" target="_blank">GitHub</Card.Link>
           </Card.Body>
         </Card>
+
         <Card style={{ width: '25rem' }}>
           <Card.Body>
             <Card.Title>Note Taker App</Card.Title>
+            <CardImg src={noteTakerPicture} />
+            <Card.Text>
+              JavaScript Application that allows a user to create and store notes.
+            </Card.Text>
+          </Card.Body>
+          <Card.Body>
+            <Card.Link href="https://note-taker-appjs.herokuapp.com/" target="_blank">Deployed Site</Card.Link>
+            <Card.Link href="https://github.com/jsimps39/note-taker-appjs" target="_blank">GitHub</Card.Link>
+          </Card.Body>
+        </Card>
+
+        <Card style={{ width: '25rem' }}>
+          <Card.Body>
+            <Card.Title>Mern Investment App</Card.Title>
+            <Card.Text>
+              JavaScript Application that allows a user to create and store notes.
+            </Card.Text>
+          </Card.Body>
+          <Card.Body>
+            <Card.Link href="https://mysterious-gorge-28306.herokuapp.com" target="_blank">Deployed Site</Card.Link>
+            <Card.Link href="https://github.com/jsimps39/mern-investment-app" target="_blank">GitHub</Card.Link>
+          </Card.Body>
+        </Card>
+
+        <Card style={{ width: '25rem' }}>
+          <Card.Body>
+            <Card.Title>Coming Soon!</Card.Title>
             <Card.Text>
               JavaScript Application that allows a user to create and store notes.
             </Card.Text>
